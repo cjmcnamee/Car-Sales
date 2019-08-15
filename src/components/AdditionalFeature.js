5bb4b967359db95a19a7927ac3803244906cae51
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-08-15 12:04:11 -0400

    reached mvp thanks to someone else's code; would've never completed this assignment otherwise

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 
 const AdditionalFeature = props => {
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={(e) => {
+        e.stopPropagation()
+        props.buyItem(props.feature)
+      }} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
